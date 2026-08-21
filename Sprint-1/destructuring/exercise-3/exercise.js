515e4e44394b1ca013ec35590fd6e7aba4ae0b90
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-21 15:11:53 +0100

    update exercise 3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,11 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+let sum = 0;
+console.log("QTY     ITEM                TOTAL");
+for (let {itemName, quantity, unitPricePence} of order) {
+  sum = sum+unitPricePence;
+  console.log(`${quantity}\t${itemName.padEnd(20, " ")}${(unitPricePence/100).toFixed(2)}`);
+}
+console.log("\nTotal:", (sum/100).toFixed(2));
