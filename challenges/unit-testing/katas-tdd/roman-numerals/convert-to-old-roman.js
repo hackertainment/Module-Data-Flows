e62221c5f671fa53324816f6b0cbf697cd3dd1b2
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-26 18:02:18 +0100

    unit testing convert to old roman

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,21 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+    let roman = "";
+
+    roman += "M".repeat(Math.floor(n/1000));
+    n = n%1000;
+    roman += "D".repeat(Math.floor(n/500));
+    n = n%500;
+    roman += "C".repeat(Math.floor(n/100));
+    n = n%100;
+    roman += "L".repeat(Math.floor(n/50));
+    n = n%50;
+    roman += "X".repeat(Math.floor(n/10));
+    n = n%10;
+    roman += "V".repeat(Math.floor(n/5));
+    n = n%5;
+    roman += "I".repeat(Math.floor(n));
+
+    return roman;
+}
 
 module.exports = convertToOldRoman;
