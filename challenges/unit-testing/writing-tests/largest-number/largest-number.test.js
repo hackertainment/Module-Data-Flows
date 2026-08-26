a3519aec846b9fa8334e9ebecf43c68d3879cf90
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-26 18:46:26 +0100

    writing largest number tests

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -4,6 +4,11 @@ test("returns largest number in array", function () {
   // Arrange
   // Act
   // Assert
+    expect(getLargestNumber([0])).toEqual(0);
+    expect(getLargestNumber([5])).toEqual(5);
+    expect(getLargestNumber([-5])).toEqual(-5);
+    expect(getLargestNumber([3, 21, 88, 4, 36])).toEqual(88);
+    expect(getLargestNumber([-3, -21, -88, -4, -36])).toEqual(-3);    
 });
 
 // example
@@ -11,3 +16,8 @@ test("returns largest number in array", function () {
 // expected: 88;
 
 // also test that the original array hasn't changed
+test("the original array hasn't changed", function () {
+    nums = [3, 21, 88, 4, 36];
+    getLargestNumber(nums);
+    expect(nums).toEqual([3, 21, 88, 4, 36]);
+});
