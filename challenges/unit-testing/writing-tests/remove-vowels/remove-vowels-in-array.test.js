a436d11aebd03bde23363b88da346bb7c3be4f83
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-26 18:56:23 +0100

    writing remove vowels in array tests

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -4,6 +4,13 @@ test("remove vowels from all words in array", function () {
   // Arrange
   // Act
   // Assert
+    expect(removeVowelsFromWords([])).toEqual([]);
+    expect(removeVowelsFromWords([""])).toEqual([""]);
+    expect(removeVowelsFromWords(["ABCDEFGHIJKLMNOPQRSTUVWXYZ"])).toEqual(["BCDFGHJKLMNPQRSTVWXYZ"]);
+    expect(removeVowelsFromWords(["abcdefghijklmnopqrstuvwxyz"])).toEqual(["bcdfghjklmnpqrstvwxyz"]);
+    expect(removeVowelsFromWords(["AEIOU", "aeiou"])).toEqual(["", ""]);
+    expect(removeVowelsFromWords(["BCDFGHJKLMNPQRSTVWXYZ", "bcdfghjklmnpqrstvwxyz"])).toEqual(["BCDFGHJKLMNPQRSTVWXYZ", "bcdfghjklmnpqrstvwxyz"]);
+    expect(removeVowelsFromWords(["Irina", "Etza", "Daniel"])).toEqual(["rn", "tz", "Dnl"]);
 });
 
 // example
